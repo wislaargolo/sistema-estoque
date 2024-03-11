1b7a0e9ec513e49d71872e22878591298fd86d4e
wislaargolo <wislaargolo1@gmail.com>
2024-03-11 11:15:56 -0300

    inicializando projeto

---
+++ src/main/java/br/ufrn/imd/sistemaestoque/SistemaEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.sistemaestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstoqueApplication.class, args);
+	}
+
+}
